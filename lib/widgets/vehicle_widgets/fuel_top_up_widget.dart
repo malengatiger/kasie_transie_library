7937b811e09800b2817041c760c60248016effa9
aubreymalabie <aubrey@iqlab.africa>
2025-01-30 20:11:07 +0200

    😡 added fuel top up

--- lib/widgets/vehicle_widgets/fuel_top_up_widget.dart
+++ lib/widgets/vehicle_widgets/fuel_top_up_widget.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
+import 'package:intl/intl.dart';
 import 'package:kasie_transie_library/bloc/data_api_dog.dart';
 import 'package:kasie_transie_library/bloc/list_api_dog.dart';
 import 'package:kasie_transie_library/data/data_schemas.dart';
 import 'package:kasie_transie_library/utils/device_location_bloc.dart';
 import 'package:kasie_transie_library/utils/functions.dart';
-import 'package:intl/intl.dart';
+
 import '../../utils/prefs.dart';
 
 class FuelTopUpWidget extends StatefulWidget {
-  const FuelTopUpWidget({Key? key, required this.vehicle}) : super(key: key);
+  const FuelTopUpWidget(
+      {Key? key, required this.vehicle, required this.isLandscape})
+      : super(key: key);
 
   final Vehicle vehicle;
+  final bool isLandscape;
 
   @override
   FuelTopUpWidgetState createState() => FuelTopUpWidgetState();
@@ -59,35 +64,37 @@ class FuelTopUpWidgetState extends State<FuelTopUpWidget>
     });
   }
 
-
   _submit() async {
     setState(() {
       busy = true;
     });
-    pp('$mm ... submit ...');
+    pp('\n\n$mm ... ........... submit ...');
     try {
       var user = prefs.getUser();
       var loc = await dlb.getLocation();
       var pos =
           Position(coordinates: [loc.longitude, loc.latitude], type: 'Point');
+      pp('$mm ... ........... create FuelTopUp ...');
+
       var ft = FuelTopUp(
           fuelTopUpId: DateTime.now().toUtc().toIso8601String(),
           vehicleId: widget.vehicle.vehicleId!,
           vehicleReg: widget.vehicle.vehicleReg!,
           fuelBrandId: fuelBrand!.fuelBrandId,
           brandName: fuelBrand!.brandName,
-          userId: user!.userId,
-          userName: '${user.firstName} ${user.lastName}',
-          associationId: user.associationId,
-          associationName: user.associationName,
+          userId: widget.isLandscape? null: user!.userId,
+          userName:  widget.isLandscape? null: '${user!.firstName} ${user.lastName}',
+          associationId: widget.vehicle.associationId,
+          associationName: widget.vehicle.associationName,
           amount: amount,
           numberOfLitres: litres,
           position: pos);
-      var res = await dataApiDog.addFuelTopUp(ft);
-      pp('$mm ... re: ${res.toJson()}');
+      pp('$mm ... ........... send FuelTopUp ...');
+      var result = await dataApiDog.addFuelTopUp(ft);
+      pp('$mm ... result: ${result.toJson()}');
       showClose = true;
     } catch (e, s) {
-      pp('$e $s');
+      pp('$mm $e $s');
       if (mounted) {
         showErrorToast(message: 'Failed to submit: $e', context: context);
       }
@@ -114,21 +121,33 @@ class FuelTopUpWidgetState extends State<FuelTopUpWidget>
           return AlertDialog(
               title: Text(
                 "${widget.vehicle.vehicleReg}",
-                style: myTextStyleBold(fontSize: 32),
+                style: myTextStyleBold(fontSize: widget.isLandscape ? 16 : 24),
               ),
-              content: FuelForm(
-                litresController: litresController,
-                amountController: amountController,
-                onSubmit: () {
-                  amount = double.parse(amountController.text);
-                  litres = double.parse(litresController.text);
-                  setState(() {});
-                  Navigator.of(context).pop();
-                  _submit();
-                },
-                formKey: formKey,
-                fuelBrand: fuelBrand!,
-              ),
+              content: widget.isLandscape
+                  ? FuelFormLandscape(
+                      litresController: litresController,
+                      amountController: amountController,
+                      onSubmit: () {
+                        setState(() {});
+                        Navigator.of(context).pop();
+                        pp('$mm calling submit from dialog');
+                        _submit();
+                      },
+                      formKey: formKey,
+                      fuelBrand: fuelBrand!)
+                  : FuelForm(
+                      litresController: litresController,
+                      amountController: amountController,
+                      onSubmit: () {
+                        amount = double.parse(amountController.text);
+                        litres = double.parse(litresController.text);
+                        setState(() {});
+                        Navigator.of(context).pop();
+                        _submit();
+                      },
+                      formKey: formKey,
+                      fuelBrand: fuelBrand!,
+                    ),
               actions: [
                 TextButton(
                     onPressed: () {
@@ -146,6 +165,12 @@ class FuelTopUpWidgetState extends State<FuelTopUpWidget>
 
   @override
   Widget build(BuildContext context) {
+    if (widget.isLandscape) {
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.landscapeRight,
+        DeviceOrientation.landscapeLeft,
+      ]);
+    }
     return Scaffold(
         appBar: AppBar(
           title: const Text('Fuel TopUp ...'),
@@ -160,6 +185,7 @@ class FuelTopUpWidgetState extends State<FuelTopUpWidget>
                 padding: EdgeInsets.all(16),
                 child: BrandSelector(
                     fuelBrands: fuelBrands,
+                    isLandscape: widget.isLandscape,
                     onPicked: (brand) {
                       pp('$mm tapped: ${brand.toJson()}');
                       setState(() {
@@ -168,9 +194,9 @@ class FuelTopUpWidgetState extends State<FuelTopUpWidget>
                       _displayForm();
                     }),
               ),
-              gapH32,
-              gapH32,
-              gapH32,
+              widget.isLandscape ? SizedBox() : gapH32,
+              widget.isLandscape ? SizedBox() : gapH32,
+              widget.isLandscape ? SizedBox() : gapH32,
               fuelBrand == null
                   ? gapW32
                   : Padding(
@@ -241,9 +267,11 @@ class FuelTopUpWidgetState extends State<FuelTopUpWidget>
               ? Positioned(
                   bottom: 16,
                   right: 16,
-                  child: TextButton(onPressed: () {
-                    Navigator.of(context).pop();
-                  }, child: Text('Done')))
+                  child: TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('Done')))
               : gapW32,
           busy
               ? Positioned(
@@ -263,41 +291,48 @@ class FuelTopUpWidgetState extends State<FuelTopUpWidget>
 
 class BrandSelector extends StatelessWidget {
   const BrandSelector(
-      {super.key, required this.fuelBrands, required this.onPicked});
+      {super.key,
+      required this.fuelBrands,
+      required this.onPicked,
+      required this.isLandscape});
 
   final List<FuelBrand> fuelBrands;
   final Function(FuelBrand) onPicked;
+  final bool isLandscape;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-        elevation: 2,
-        child: Padding(
-            padding: EdgeInsets.all(16),
-            child: SizedBox(
-                height: 200,
-                child: GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 3),
-                    itemCount: fuelBrands.length,
-                    itemBuilder: (_, index) {
-                      var b = fuelBrands[index];
-                      return Column(
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              pp(' ... onPicked: ${b.toJson()}');
-                              onPicked(b);
-                            },
-                            child: CircleAvatar(
-                              backgroundImage: NetworkImage(b.logoUrl!),
-                            ),
-                          ),
-                          Text(b.brandName!),
-                        ],
-                      );
-                    }))));
+      elevation: 2,
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: SizedBox(
+          height: isLandscape ? 64 : 200,
+          child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: isLandscape ? 6 : 3),
+              itemCount: fuelBrands.length,
+              itemBuilder: (_, index) {
+                var b = fuelBrands[index];
+                return Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        pp(' ... onPicked: ${b.toJson()}');
+                        onPicked(b);
+                      },
+                      child: CircleAvatar(
+                        radius: 24,
+                        backgroundImage: NetworkImage(b.logoUrl!),
+                      ),
+                    ),
+                    Text(b.brandName!),
+                  ],
+                );
+              }),
+        ),
+      ),
+    );
   }
 }
 
@@ -368,6 +403,80 @@ class FuelForm extends StatelessWidget {
                         Text('Submit', style: myTextStyle(color: Colors.white)),
                   ),
                 )
+              ],
+            )));
+  }
+}
+
+class FuelFormLandscape extends StatelessWidget {
+  const FuelFormLandscape(
+      {super.key,
+      required this.litresController,
+      required this.amountController,
+      required this.onSubmit,
+      required this.formKey,
+      required this.fuelBrand});
+
+  final TextEditingController litresController, amountController;
+  final Function() onSubmit;
+  final GlobalKey<FormState> formKey;
+  final FuelBrand fuelBrand;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 340,
+        child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: 200,
+                      child: TextFormField(
+                        controller: litresController,
+                        keyboardType:
+                            TextInputType.numberWithOptions(decimal: true),
+                        style: myTextStyleBold(fontSize: 20),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Please enter litres',
+                          label: Text('Litres of Fuel'),
+                        ),
+                      ),
+                    ),
+                    gapW32,
+                    SizedBox(
+                      width: 200,
+                      child: TextFormField(
+                        controller: amountController,
+                        keyboardType:
+                            TextInputType.numberWithOptions(decimal: true),
+                        style: myTextStyleBold(fontSize: 28),
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Please enter amount',
+                            label: Text('Total Amount')),
+                      ),
+                    )
+                  ],
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                      elevation: WidgetStatePropertyAll(12),
+                      backgroundColor: WidgetStatePropertyAll(Colors.blue)),
+                  onPressed: () {
+                    onSubmit();
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child:
+                        Text('Submit TopUp', style: myTextStyle(color: Colors.white)),
+                  ),
+                ),
+
               ],
             )));
   }
